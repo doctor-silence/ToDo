5cb2089c589d8209997bb51f9769716bdb68c515
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-10-17 15:56:19 +0700

    add_current

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,26 @@
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+
 
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=
+            try:
+                user = User.objects.create_user(request.POST['username'], password=
                                             request.POST['password1'])
-            user.save()
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': 'Это имя пользователя уже используется'})
         else:
-            print('Привет')
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Пароль не совпадает'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
