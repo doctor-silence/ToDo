5eb5ba2c758da28b10dc51c3a5397b9f535227d9
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-10-19 21:27:46 +0700

    add_view

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
 
-
+def home(request):
+    return render(request, 'todo/home.html')
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
@@ -21,6 +22,21 @@ def signupuser(request):
                               {'form': UserCreationForm(), 'error': 'Это имя пользователя уже используется'})
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Пароль не совпадает'})
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error':'Имя или пароль не верный'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
