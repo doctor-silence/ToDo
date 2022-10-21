e50bc899fc126cbf175100194828d7851f5ee175
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-10-21 15:24:01 +0700

    add_model

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-
-
-class ToDo(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=250)
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
