7db98b75fe1c57c8cdab6b515a4d36d15d4b94e1
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-10-22 18:53:15 +0700

    add_admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):  #Добавляем дату создания
+    readonly_fields = ('created',)
+
+admin.site.register(Todo, TodoAdmin)
